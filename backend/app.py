2b880d4f2aa8f2abee7f36d30d552d50db7d24ea
Turhan Akira <tkimbr1@students.towson.edu>
2024-11-30 15:22:37 -0500

    made progress on microservice

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI, APIRouter
+import auto_categorize_notes
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='Note-a-log AI Services',
-    description='Provides GenAI capabilities for the Note-a-log app.'
+    description='Provides GenAI capabilities for the Note-a-log app.',
 )
 
 # CORS Configuration
 origins = [
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
 ]
 
 app.add_middleware(
@@ -20,6 +21,16 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-@app.get('/')
-def root():
-    return {'message': 'Welcome to the FastAPI microservice!'}
+@app.get('/auto_categorize_notes')
+def auto_categorize():
+    """
+        Calls the auto_categorize_notes script and handles exceptions.
+
+        Returns a status message indicating success or failure.
+    """
+    try:
+        auto_categorize_notes.main()
+        return {"status": "success", "message": "Notes categorized successfully."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'An error occurred: {str(e)}')
+
