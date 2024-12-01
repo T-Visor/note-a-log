da464c27c46f7e9ac87a6ec44d7041eaf77d7918
Turhan Akira <tkimbr1@students.towson.edu>
2024-12-01 17:30:24 -0500

    moved to the larger llama3.1 8b model since it performs better

--- backend/config.py
+++ backend/config.py
@@ -2,13 +2,13 @@ DATABASE_PATH = '../data/notesApp.db'
 DATABASE_TABLE_WITH_NOTES = 'notes'
 DATABASE_TABLE_WITH_FOLDERS = 'folders' 
 
-MODEL_NAME = 'llama3.2'
+MODEL_NAME = 'llama3.1:8b'
 
 PROMPT_TEMPLATE = """
 ### Note Categorization
 
 Based on the following title and content from a note and potential existing set of
-categories, suggest a concise category name for the note without explanation:
+categories, suggest a concise category name in plain-text for the note without explanation:
 
 **Title:** {{ title }} 
 **Content:** {{ content }} 
