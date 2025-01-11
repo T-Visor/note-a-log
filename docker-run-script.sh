6f8eefd33f12de63126d136a491497cc39e80bea
T-Visor <turkimbro@yahoo.com>
2025-01-11 15:16:15 -0500

    updated commands for docker distribution script

--- docker-run-script.sh
+++ docker-run-script.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Create the network for the containers to communicate
+docker network create note-a-log-network
+
 # Build the container locally
 docker build --no-cache -t my-nextjs-app .
 
 # Run the container
-#docker run -p 3000:3000 -v $(pwd)/data/notesApp.db:/data/notesApp.db my-nextjs-app
-#docker run -p 3000:3000 -p 8000:8000 --add-host=host.docker.internal:host-gateway -v $(pwd)/data/notesApp.db:/app/data/notesApp.db my-nextjs-app
 docker run -d --network note-a-log-network --env-file .env.docker -p 3000:3000 -p 8000:8000 -v $(pwd)/data/notesApp.db:/app/data/notesApp.db my-nextjs-app
 
 # Run ollama
 docker run -d --network note-a-log-network -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+docker exec ollama ollama pull llama3.1:8b-instruct-q3_K_S
