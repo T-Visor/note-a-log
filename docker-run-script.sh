beab1fc30818b97c1c7be4530e1e3e619b4bd9c2
T-Visor <turkimbro@yahoo.com>
2025-02-02 15:06:57 -0500

    modified code so that only localhost can access application instead of local network

--- docker-run-script.sh
+++ docker-run-script.sh
@@ -10,7 +10,7 @@ docker volume create notesAppData
 docker build --no-cache -t my-nextjs-app .
 
 # Run the container for Note-a-log
-docker run -d --network note-a-log-network --env-file .env.docker -p 3000:3000 -p 8000:8000 -v notesAppData:/app/data --name note-a-log my-nextjs-app
+docker run -d --network note-a-log-network --env-file .env.docker -p 127.0.0.1:3000:3000 -p 127.0.0.1:8000:8000 -v notesAppData:/app/data --name note-a-log my-nextjs-app
 
 # Run Ollama
 docker run -d --network note-a-log-network -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
