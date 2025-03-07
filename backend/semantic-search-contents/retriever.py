85202d422495d8396583606fe5961f26959d9504
T-Visor <turkimbro@yahoo.com>
2025-03-07 13:58:50 -0500

    updated top-k to 3 samples instead of 10

--- backend/semantic-search-contents/retriever.py
+++ backend/semantic-search-contents/retriever.py
@@ -62,8 +62,9 @@ if __name__ == "__main__":
 
     #query_text = "On 15,000 mile interval: oil change, tire rotation, check engine filter, lubricate hinges, change drain plug washer"
     query_text = "Go to Wegmans grocery store on Tuesday"
+    top_k = 3
 
-    retrieved_docs = retriever.query(query_text)
+    retrieved_docs = retriever.query(query_text, top_k)
 
     print(f"Query: {query_text}\n")
 
