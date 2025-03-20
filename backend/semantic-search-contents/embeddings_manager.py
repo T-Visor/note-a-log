e10384387baeae11a2a405d835c1b5d08714ee6c
Turhan Akira <turkimbro@yahoo.com>
2025-03-20 14:15:00 -0400

    created class for updating AND deleting embeddings, need to integrate with API now

--- backend/semantic-search-contents/embeddings_manager.py
+++ backend/semantic-search-contents/embeddings_manager.py
@@ -14,8 +14,16 @@ from config import (
 from markdown_to_plain import strip_markdown
 
 
-class EmbeddingsUpdater:
+class EmbeddingsManager:
+    """
+    Manages embeddings for documents stored in Qdrant.
+    Provides methods to update and delete document embeddings.
+    """
+    
     def __init__(self):
+        """
+            Initializes the embeddings manager with document store and embedding models.
+        """
         self.qdrant_document_store = QdrantDocumentStore(**QDRANT_CONFIG)
         self.sparse_text_embedder = FastembedSparseTextEmbedder(
             model=FASTEMBED_SPARSE_MODEL, cache_dir=FASTEMBED_CACHE_DIRECTORY
@@ -27,7 +35,17 @@ class EmbeddingsUpdater:
             document_store=self.qdrant_document_store, policy=DuplicatePolicy.OVERWRITE
         )
     
+
     def update_embedding(self, document_id: str, new_content: str):
+        """
+            Updates the embedding for a given document ID by recomputing its embeddings
+            using dense and sparse models.
+        
+        :param document_id: The unique identifier of the document.
+        :param new_content: The new text content for which embeddings will be computed.
+
+        :return: The updated document.
+        """
         # Retrieve the document
         documents = self.qdrant_document_store.get_documents_by_id([document_id])
         if not documents:
@@ -49,7 +67,18 @@ class EmbeddingsUpdater:
         return document
 
 
+    def delete_embedding(self, document_id: str):
+        """
+            Deletes the embedding of a given document ID from the document store.
+        
+        :param document_id: The unique identifier of the document to be deleted.
+        """
+        self.qdrant_document_store.delete_documents([document_id])
+        print(f"Deleted document with ID: {document_id}")
+
+
 # Example usage:
-updater = EmbeddingsUpdater()
-updater.update_embedding('bf1d37b33071ea26e170bc3583dce41cddf70305d7356bc6ee3365363e1282de', "Hampton inn complete!")
+# updater = EmbeddingsManager()
+# updater.update_embedding('bf1d37b33071ea26e170bc3583dce41cddf70305d7356bc6ee3365363e1282de', "Hampton inn complete!")
+# updater.delete_embedding('f94e39573a1217ed0e299082a88ff22a4a9bae31a8bc7191855059bd82c35bfc')
 
