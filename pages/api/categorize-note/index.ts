eedc3aa6cc0b963b7672674e0a44438550a614a8
T-Visor <turkimbro@yahoo.com>
2025-06-25 23:02:25 -0400

    started coding implementation of categorization

--- pages/api/categorize-note/index.ts
+++ pages/api/categorize-note/index.ts
@@ -1,5 +1,13 @@
 import { categorizeNoteWithAI } from '@/lib/categorize-notes';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { useNotes } from '@/hooks/useNotes';
+import { Note, Folder } from "@/types";
+
+interface NoteWithSuggestedFolderMove {
+  note: Note;
+  folderName: string;
+  folderId: string | null;
+}
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   if (request.method === 'POST') {
@@ -11,4 +19,16 @@ export default async function handler(request: NextApiRequest, response: NextApi
     response.setHeader('Allow', ['POST']);
     response.status(405).end(`Method ${request.method} Not Allowed`);
   }
+}
+
+const main = async () => {
+  const { notes, folders, handleMoveNote, handleNewFolder } = useNotes();
+
+  const unassignedNotes = notes.filter(note => note.folderId === "unassigned");
+
+  const results = await Promise.all(
+    unassignedNotes.map(note => 
+      categorizeNoteWithAI(note.title, note.content, note.embeddingsId!)
+    )
+  )
 }
